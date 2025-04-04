30096abed53354e480502d9a7b688001a4b3644b
mercedesferrando <mferrando@mail.austral.edu.ar>
2025-04-04 12:31:45 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    fullname = f"{first_name} {last_name}"
+    print(fullname.lower())
+    print(fullname.title())
+    print(fullname.upper())
+    print('\t'+fullname.lower())
+
+ada()
